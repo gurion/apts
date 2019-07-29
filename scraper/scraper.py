4775727400d81386f61c10a802e6a4d57bc1f107
Gurion Marks <gurion@jhu.edu>
2019-07-29 11:18:43 -0400

    Get list of listing ids

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -17,7 +17,12 @@ def get_url_list():
     '''return a list of property URLs to parse in a given polygon'''
     soup = get_page(BASE_URL + extension)
     page_urls = get_all_page_urls(soup)
-    print(get_all_properties(soup))
+    listing_ids = []
+    for url in page_urls:
+        page = get_page(url)
+        listing_ids += get_property_ids_from_page(page)
+
+    return listing_ids
 
 
 def get_page(url):
@@ -27,38 +32,29 @@ def get_page(url):
 
 
 def get_all_page_urls(soup):
-    '''
-    Get all the urls needed to iterate over all the placards
-    '''
+    '''Get all the urls needed to iterate over all the placards'''
     pages = soup.find('div', {'id': 'placardContainer'}).find('div', {'id': 'paging'}).find_all('a')
     start_page = pages[1].text
     last_page = pages[len(pages)-2].text
     return [BASE_URL+extension+str(page_number) for page_number in range(int(start_page), int(last_page) + 1)]
 
 
-def get_all_properties(soup):
+def get_property_ids_from_page(soup):
     '''get list of property listing IDs'''
-    placards = soup.find('div', {'id': 'placardContainer'}).find('ul').find_all('li')
-    for element in placards:
-        continue
+    placards = soup.find('div', {'id': 'placardContainer'}).find_all('article', {})
+    return [placard['data-listingid'] for placard in placards]
 
 
 def write_csv():
-    '''
-        write data structure (in format in testing/data_structure.json) to a csv file
-    '''
+    '''write data structure (in format in testing/data_structure.json) to a csv file'''
 
 
 def create_csv_file():
-    '''
-        create a csv file for writing data
-    '''
+    '''create a csv file for writing data'''
 
 
 def get_policy():
-    '''
-        Get building policies - parking, gym, etc.
-    '''
+    '''Get building policies - parking, gym, etc.'''
 
 
 def get_rent(table_row):
