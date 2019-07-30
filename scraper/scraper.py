29bbea27aec6bae85335fafafeb79cf9a6049e0f
Gurion Marks <gurion@jhu.edu>
2019-07-30 16:10:53 -0400

    refactored address and url extraction need to error handle tables on web pages

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,11 +7,13 @@ import pandas
 
 ########## GLOBAL VARS #######################
 BASE_URL = 'https://www.apartments.com/'
-extension = 'new-york-ny/'  # This is your unique search identifier from a region
+# This is your unique search identifier from a region
+extension = '?sk=5cad1a13429d27fcc6faa34b11b0ddd1&bb=yskr9wknwHzl3_uB/'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
 }
-data = {'properties': {}}
+data = {}
+url_dict = {}
 ##############################################
 
 
@@ -19,12 +21,8 @@ def get_url_list():
     '''return a list of property URLs to parse in a given polygon'''
     soup = get_page_soup(BASE_URL + extension)
     page_urls = get_all_page_urls(soup)
-    listing_ids = []
     for url in page_urls:
-        page = get_page_soup(url)
-        listing_ids += get_property_ids_from_search_page(page)
-
-    return listing_ids
+        get_property_addresses_and_urls_from_search_page(get_page_soup(url))
 
 
 def get_page_soup(url):
@@ -35,41 +33,41 @@ def get_page_soup(url):
 
 def get_all_page_urls(soup):
     '''Get all the urls needed to iterate over all the placards'''
-    pages = soup.find('div', {'id': 'placardContainer'}).find('div', {'id': 'paging'}).find_all('a')
-    start_page = pages[1].text
-    last_page = pages[len(pages)-2].text
-    return [BASE_URL+extension+str(page_number) for page_number in range(int(start_page), int(last_page) + 1)]
+    try:
+        pages = soup.find('div', {'id': 'placardContainer'}).find(
+            'div', {'id': 'paging'}).find_all('a')
+        start_page = pages[1].text
+        last_page = pages[len(pages)-2].text
+        return [BASE_URL+extension+str(page_number) for page_number in range(int(start_page), int(last_page) + 1)]
+    except AttributeError:
+        return [BASE_URL+extension]
 
 
-def get_property_ids_from_search_page(soup):
-    '''get list of property listing IDs'''
-    placards = soup.find('div', {'id': 'placardContainer'}).find_all('article', {})
-    return [placard['data-listingid'] for placard in placards]
+def get_property_addresses_and_urls_from_search_page(soup):
+    '''get list of listing urls associated with property address'''
+    script = soup.find('div', {'class': 'mainWrapper'}).find(
+        'script', type='application/ld+json').get_text()
+    json_data = json.loads(script)['about']
+    properties = {property['url']: get_property_address_from_json(
+        property) for property in json_data}
+    url_dict.update(properties)
 
 
-def get_property_address(soup):
-    '''given page, extract address'''
-    script = soup.find_all('script', type='text/javascript')[2].text
-    address = find_tag(script, 'listingAddress') + ', ' + find_tag(script, 'listingCity') + \
-        ', ' + find_tag(script, 'listingState') + ' ' + find_tag(script, 'listingZip')
-    return address
+def get_property_address_from_json(property):
+    '''given json, extract address'''
+    return property['Address']['streetAddress'] + ', ' + property['Address']['addressLocality'] + ', ' + property['Address']['addressRegion'] + ' ' + property['Address']['postalCode']
 
 
-def find_tag(text, tag):
-    '''helper method for get_property_address'''
-    tag = tag + ": \'"
-    start = text.find(tag) + len(tag)
-    end = text.find("\',", start)
-    return str(text[start: end])
-
-
-def get_building_data(url):
+def add_building_data(address, url):
     '''Get the data from the table of apartments associated with the building'''
     soup = get_page_soup(url)
-    address = get_property_address(soup)
-    add_address(address)
-    table_rows = soup.find('div', {'id': 'apartmentsTabContainer'}).find(
-        'div', {'class': 'js-expandableContainer'}).find('tbody').find_all('tr')
+    address = url_dict[url]
+    table_rows = []
+    try:
+        table_rows = soup.find('div', {'id': 'apartmentsTabContainer'}).find(
+            'div', {'class': 'js-expandableContainer'}).find('tbody').find_all('tr')
+    except:
+        print(url)
 
     for tr in table_rows:
         unit = get_unit(tr)
@@ -100,7 +98,10 @@ def get_beds(tr):
 def get_baths(tr):
     '''get number of bathrooms'''
     baths = tr.find('td', {'class': 'baths'}).text.split()[0]
-    return float(baths)
+    try:
+        return float(baths)
+    except ValueError:
+        return float(baths[0]) + .5
 
 
 def get_rent(tr):
@@ -113,10 +114,10 @@ def get_rent(tr):
 
 def get_sqft(tr):
     '''get square footage'''
-    sqft = tr.find('td', {'class': 'sqft'}).text
+    sqft = tr.find('td', {'class': 'sqft'}).get_text().split()[0]
     if sqft == '':
         return -1
-    return int(sqft.text.strip().replace(',', ''))
+    return int(sqft.strip().replace(',', ''))
 
 
 def get_avail(tr):
@@ -130,10 +131,10 @@ def get_avail(tr):
 
 def add_address(address):
     '''add building to database'''
-    data['properties'].update({
+    data.update({
         address: {
-            'units': [],
-            'policies': {
+            "units": [],
+            "policies": {
                 "pets": "TBD",
                 "parking": "TBD",
                 "property_info": "TBD",
@@ -146,7 +147,7 @@ def add_address(address):
 
 def add_unit(address, unit_num='', beds=-1, baths=-1.0, rent=-1, sqft=-1, avail=-1):
     '''add a unit to the database'''
-    data["properties"][address]["units"].append({
+    data[address]["units"].append({
         "unit": unit_num,
         "beds": beds,
         "baths": baths,
@@ -154,6 +155,13 @@ def add_unit(address, unit_num='', beds=-1, baths=-1.0, rent=-1, sqft=-1, avail=
         "sqft": sqft,
         "avail": avail
     })
+
+
+def scrape():
+    for url in url_dict:
+        address = url_dict[url]
+        add_address(address)
+        add_building_data(address, url)
 
 
 def write_csv():
@@ -167,7 +175,8 @@ def create_csv_file():
 def main():
     '''
     '''
-    get_building_data("https://www.apartments.com/sky-new-york-ny/w1h7edh/")
+    get_url_list()
+    scrape()
     print(data)
 
 
