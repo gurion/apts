77cc54214a7aec01350ffb64a7d499d706e21ed2
Gurion Marks <gurion@jhu.edu>
2019-08-02 00:08:32 -0400

    Collect amenity data

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,7 +5,11 @@ import json
 import re
 from tqdm import tqdm
 
-########## GLOBAL VARS #######################
+################################################################################
+# Global Vars
+################################################################################
+
+
 BASE_URL = 'https://www.apartments.com/'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
@@ -13,7 +17,11 @@ headers = {
 data = {}
 url_dict = {}
 errors = {}
-##############################################
+
+
+################################################################################
+# Getting URLs and page data
+################################################################################
 
 
 def fill_url_dict(extension):
@@ -76,6 +84,8 @@ def get_property_address_from_json(property):
 def add_building_data(address, url):
     '''Get the data from the table of apartments associated with the building'''
     soup = get_page_soup(url)
+    pets, parking, built, renovated, num_units, stories, fitness, outdoor = get_amenity_info(soup)
+    add_building_policies(address, pets, parking, built, renovated, num_units, stories, fitness, outdoor)
     table_rows = []
     try:
         table_rows = soup.find('div', {'id': 'apartmentsTabContainer'}).find(
@@ -85,30 +95,24 @@ def add_building_data(address, url):
             table_rows = soup.find('section', {'id': 'availabilitySection'}).find(
                 'tbody').find_all('tr', {'class': 'rentalGridRow'})
         except Exception as e:
-            errors.update({
-                e: url
-            })
+            errors.update({e: url})
 
     for tr in table_rows:
         parse_row(tr, url)
 
 
+################################################################################
+# Getting data for a specific apartment
+################################################################################
+
+
 def parse_row(row, url):
     '''get relevant data from a table row'''
     try:
-        add_unit(
-            url_dict[url],
-            get_unit_name(row),
-            get_beds(row),
-            get_baths(row),
-            get_rent(row),
-            get_sqft(row),
-            get_avail(row)
-        )
+        add_unit(url_dict[url], get_unit_name(row), get_beds(row),
+                 get_baths(row), get_rent(row), get_sqft(row), get_avail(row))
     except Exception as e:
-        errors.update({
-            e: url
-        })
+        errors.update({e: url})
 
 
 def get_unit_name(tr):
@@ -165,19 +169,100 @@ def get_avail(tr):
     else:
         return 0
 
+################################################################################
+# Getting Amenity Info
+################################################################################
+
+
+def get_amenity_info(soup):
+    '''get building amenity info from page'''
+    pets = get_pet_info(soup)
+    parking = get_parking_info(soup)
+    built, renovated, num_units, stories = get_building_info(soup)
+    fitness = get_fitness_info(soup)
+    outdoor = get_outdoor_info(soup)
+    return pets, parking, built, renovated, num_units, stories, fitness, outdoor
+
+
+def get_pet_info(soup):
+    '''get building pet policy'''
+    try:
+        policies = soup.find_all('div', {'class': 'petPolicyDetails'})
+        return ';'.join([policy.find('span').text.strip() for policy in policies])
+    except Exception as e:
+        return 'N/A'
+
+
+def get_parking_info(soup):
+    '''get parking info for a building'''
+    try:
+        policies = soup.find_all('div', {'class': 'parkingTypeFeeContainer'})
+        return ';'.join([policy.text.strip() for policy in policies])
+    except Exception as e:
+        return 'N/A'
+
+
+def get_building_info(soup):
+    '''get building build/reno date and num units'''
+    try:
+        building_info = {
+            'Built': 'N/A',
+            'Renovated': 'N/A',
+            'Units': 'N/A',
+            'Stories': 'N/A'
+        }
+        info = soup.find('div', {'class': 'propertyFeatures'}).find_all('li')
+        for li in info:
+            li = li.text.strip().replace('•', '')
+            add_to_building_info(building_info, li)
+        return building_info["Built"], building_info["Renovated"], building_info["Units"], building_info["Stories"]
+
+    except Exception as e:
+        return 'N/A', 'N/A', 'N/A', 'N/A'
+
+
+def add_to_building_info(dict, li):
+    '''helper for get_building_info'''
+    if 'Built' in li:
+        li = re.sub('[^0-9]', '', li)
+        dict['Built'] = li
+    elif 'Renovated' in li:
+        li = re.sub('[^0-9]', '', li)
+        dict['Renovated'] = li
+    else:
+        li = li.split('/')
+        dict['Units'] = re.sub('[^0-9]', '', li[0])
+        dict['Stories'] = re.sub('[^0-9]', '', li[1])
+
+
+def get_fitness_info(soup):
+    '''get info associated with fitness centers etc'''
+    try:
+        fitness = soup.find('i', {'class': 'fitnessIcon'}).parent.parent.find_all('li')
+        return ';'.join([li.text.replace('•', '') for li in fitness])
+    except:
+        return 'N/A'
+
+
+def get_outdoor_info(soup):
+    try:
+        outdoor = soup.find('i', {'class': 'parksIcon'}).parent.parent.find_all('li')
+        return ';'.join([li.text.replace('•', '') for li in outdoor])
+    except:
+        return 'N/A'
+
+
+################################################################################
+# Appending to dicts
+################################################################################
+
 
 def add_address(address):
     '''add building to database'''
     data.update({
         address: {
             'units': [],
-            'policies': {
-                'pets': 'TBD',
-                'parking': 'TBD',
-                'property_info': 'TBD',
-                'fitness': 'TBD',
-                'outdoor': 'TBD'
-            }
+            'policies': {}
         }
     })
 
@@ -195,6 +280,25 @@ def add_unit(address, unit_num, beds, baths, rent, sqft, avail):
     })
 
 
+def add_building_policies(address, pets, parking, built, renovated, num_units, stories, fitness, outdoor):
+    '''Add amenity info for a given address'''
+    data[address]['policies'].update({
+        'pets': pets,
+        'parking': parking,
+        'built': built,
+        'renovated': renovated,
+        'num_units': num_units,
+        'stories': stories,
+        'fitness': fitness,
+        'outdoor': outdoor
+    })
+
+
+################################################################################
+# Big picture
+################################################################################
+
+
 def scrape():
     '''get data for all apartments from each url'''
     for url in tqdm(url_dict):
@@ -206,29 +310,40 @@ def scrape():
 def write_csv():
     '''write data to a csv file'''
     with open('apts.csv', mode='w') as file:
-        fields = ['address', 'unit', 'beds', 'baths', 'rent', 'sqft', 'avail']
+        fields = ['address', 'unit', 'beds', 'baths', 'rent', 'sqft', 'avail', 'pets',
+                  'parking', 'built', 'renovated', 'num_units', 'stories', 'fitness', 'outdoor']
         writer = csv.DictWriter(file, fieldnames=fields)
 
         writer.writeheader()
         for address in tqdm(data):
             for unit in data[address]['units']:
+                unit.update(data[address]['policies'])
                 writer.writerow(unit)
 
 
+################################################################################
+# Main
+################################################################################
 def test():
-    '''code to figure minor stuff out'''
-    pass
+    '''testing minor stuff'''
+    soup = get_page_soup('https://www.apartments.com/21-wea-new-york-ny/w5rlg9k/')
+    get_fitness_info(soup)
 
 
 def main():
     '''find all the urls, scrape the data, write to csv'''
+
     extension = input('Please enter your unique search ID:\n') + '/'
+
     print('Getting all property URLs...')
     fill_url_dict(extension)
+
     print('Getting property data...')
     scrape()
+
     print('Writing CSV...')
     write_csv()
+
     if errors != {}:
         print('Some errors occurred:\n')
         for error in errors:
